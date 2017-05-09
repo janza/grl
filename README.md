fe09651e4bf659a795b409043af6a50ede5066a8
Josip Janzic <josip@jjanzic.com>
2017-05-09 23:40:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Grl - url shortener
+
+Very simple url shortener using [boltdb](https://github.com/boltdb/bolt)
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/boltdb/bolt"
+)
+
+func main() {
+	db, err := bolt.Open("grl.db", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	bucketName := []byte("Urls")
+
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket(bucketName)
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			var url bytes.Buffer
+			url.ReadFrom(r.Body)
+
+			// var idBytes []byte
+			var id uint64
+			db.Update(func(tx *bolt.Tx) error {
+				b := tx.Bucket(bucketName)
+				id, _ = b.NextSequence()
+
+				err := b.Put([]byte(strconv.Itoa(int(id))), url.Bytes())
+				return err
+			})
+
+			fmt.Fprintf(w, "%d", int(id))
+			return
+		}
+		if r.Method == "GET" {
+			db.View(func(tx *bolt.Tx) error {
+				b := tx.Bucket(bucketName)
+				id := []byte(r.URL.Path[1:])
+				v := b.Get(id)
+				if v != nil {
+					http.Redirect(w, r, string(v), http.StatusFound)
+				} else {
+					// http.NotFound(w, r)
+					w.WriteHeader(http.StatusNotFound)
+					fmt.Fprintf(w, "Not found: %s", id)
+				}
+
+				return nil
+			})
+			return
+		}
+
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "Not Allowed")
+	})
+
+	fmt.Print(http.ListenAndServe(":8080", nil))
+
+}
