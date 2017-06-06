14cbff771590d2dbf377d40df77c63a4ccfbe1bd
Josip Janzic <josip@jjanzic.com>
2017-06-06 13:43:45 +0200

    Add cli script to index

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -15,6 +16,29 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+
+	t, err := template.New("index").Parse(`#!/usr/bin/env bash
+
+# grl: command line url shortener.
+#
+# Examples:
+#     grl google.com            # paste file (name/ext will be set).
+#     echo Hello world. | ix    # read from STDIN (won't set name/ext).
+#     ix -n 1 self_destruct.txt # paste will be deleted after one read.
+#     ix -i ID hello.txt        # replace ID, if you have permission.
+#     ix -d ID
+
+url=$(curl -s -X POST '{{.}}' -d "$1")
+if type "xsel" &> /dev/null; then
+  clip="xsel -ib"
+elif type "xclip" &> /dev/null; then
+  clip="xclip -sel clip"
+else
+  clip="pbcopy"
+fi
+echo "$url" | $clip
+echo "$url"
+`)
 
 	bucketName := []byte("Urls")
 
@@ -45,6 +69,12 @@ func main() {
 			return
 		}
 		if r.Method == "GET" {
+			if r.URL.Path == "/" {
+				w.WriteHeader(http.StatusNotFound)
+				w.Header().Set("Content-Type", "text/plain")
+				t.Execute(w, r.URL.Scheme+r.Host+r.URL.RequestURI())
+				return
+			}
 			db.View(func(tx *bolt.Tx) error {
 				b := tx.Bucket(bucketName)
 				id := []byte(r.URL.Path[1:])
@@ -53,6 +83,7 @@ func main() {
 					http.Redirect(w, r, string(v), http.StatusFound)
 				} else {
 					w.WriteHeader(http.StatusNotFound)
+
 					fmt.Fprintf(w, "Not found: %s", id)
 				}
 
